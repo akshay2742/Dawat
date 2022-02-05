92acc8c7f2e90aa7789abd031c047c4b1e479cfe
Akshay Sharma <akshays274@gmail.com>
2022-02-05 23:16:54 +0530

    sorted infinite loop

--- assets/js/main_slider.js
+++ assets/js/main_slider.js
@@ -1,43 +1,63 @@
+let a = document.getElementById("slide1");
+let b = document.getElementById("slide2");
+let c = document.getElementById("slide3");
+a.style.display = "block";
+b.style.display = "none";
 let changeSlideLeft = function() {
-    let a = document.getElementById("slide1");
-    let b = document.getElementById("slide2");
-    // let c = document.getElementById("slide3");
+    
+    
     a.style.display = "block";
     b.style.display = "none";
     // c.style.display = "none";
 } 
 
 let changeSlideRight = function() {
-    let a = document.getElementById("slide1");
-    let b = document.getElementById("slide2");
     // let c = document.getElementById("slide3");
     a.style.display = "none";
     b.style.display = "block";
+    console.log('mratunjya');
     // c.style.display = "none";
 } 
 const slides =document.querySelectorAll('.slider_slides');
-let index = 0;
 
-setInterval(function(){
-   
-    while(index<2){
-        if(index==0)
-        {
-            slides[0].style.display = "block";
-            slides[1].style.display = "none";
-        }
-        else if(index==1)
-        {
-            slides[0].style.display = "none";
-            slides[1].style.display = "block";
-        }
-        console.log(index);
-        index++;
+let index = 1;
+
+setInterval(() => {
+    if (index%2==0) {
+        a.style.display = "block";
+        b.style.display = "none";
     }
-    index=0;
+    else {
+        a.style.display = "none";
+        b.style.display = "block";
+
+    }
+    index++;
+    
+},3000);
+
+// setInterval(function(){
    
-}
-,3000);
+//     if(index==0)
+//    {
+//        slides[0].style.display = "block";
+//        slides[1].style.display = "none";
+//    }
+//    else if(index==1)
+//    {
+//        slides[0].style.display = "none";
+//        slides[1].style.display = "block";
+//    }
+//    else if(index>1)
+//    {
+//        index=0;
+//    }
+   
+//    console.log(index);
+   
+//    index++;
+// }
+// ,3000);
 
 // slides[0].addEventListener('click', function(){
 //     slides[1].style.display = "none";
